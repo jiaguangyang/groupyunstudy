f0af21466e0d3d4fb763a70a1336e7856ca1de57
jiagy <752045494@qq.com>
2019-06-12 16:35:15 +0800

    框架

---
+++ queryservice/src/main/java/com/jk/queryservice/QueryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jk.queryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryserviceApplication.class, args);
+    }
+
+}
